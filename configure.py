d453e2ac7d849ea9980219afdb3e6f9829108e37
Daler2304 <110163963+Daler2304@users.noreply.github.com>
2022-11-04 12:11:15 +0500

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,5 +1,6 @@
 TOKEN = '5501106844:AAHowv_duJaS2QPWSRvwqEk5l3rYUDRc2hA'
 APP_URL='https://python-bot-data.herokuapp.com/' + TOKEN
+URI='postgres://mjspvaqmszltlv:8d870b00f05cd121f3ea189bb7f6f2874eed7dae9491cc26823ec0f210313c25@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d17v178if4666k'
 admin=5097960608
 
 
