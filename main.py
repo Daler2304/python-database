30efb4e82b37845de8bf7399b6d5f564220e593a
Daler2304 <110163963+Daler2304@users.noreply.github.com>
2022-11-10 12:04:18 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -39,16 +39,35 @@ def start(message):
 @bot.message_handler(commands=['send'])
 def send(message):
     if message.from_user.id==admin:
-        msg=bot.send_message(message.chat.id,'Введите сообщение для рассылки')
-        bot.register_next_step_handler(msg, send_message)
-
-def send_message(message):
-    cursor.execute('SELECT id FROM users')
-    for i in cursor.fetchall():
+        bot.send_message(message.chat.id,'Рассылка')
         if message.text:
-            bot.send_message(i[0],f'{message.text}')
-        elif message.photo:
-            bot.send_photo(i[0], photo=message.photo[0].file_id, caption=f'{message.text}')
+            sending_message=message.text
+            markup=types.InlineKeyboardMarkup()
+            markup.add(
+                types.InlineKeyboardButton('Добавить фото',callback_data='add_photo'),
+                types.InlineKeyboardButton('Начать рассылку', callback_data='start_sending')
+                )
+            msg=bot.send_message(message.chat.id, f'{message.text}',reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call:True)
+def call(call):
+    if call.data=='add_photo':
+        msg=bot.send_message(call.message.chat.id,'Отправьте фото')
+        bot.register_next_step_handler(msg,send_photo)
+    elif call.data=='start_sending':
+        cursor.execute('SELECT id FROM users')
+        for i in cursor.fetchall():
+            bot.send_message(i[0],f'{sending_message}')
+
+def send_photo(message):
+    if message.photo:
+        cursor.execute('SELECT id FROM users')
+        for i in cursor.fetchall():
+            bot.send_photo(i[0], photo=message.photo[0].file_id, caption=f'{sending_message}')
+    else:
+        bot.reply_to(message,'Фото не обнаружено!')
+            
 @bot.message_handler(content_types=['text'])
 def mess(message):
     id=message.from_user.id
@@ -56,7 +75,6 @@ def mess(message):
         bot.reply_to(message, f'Ваш ID: {id}')
     elif message.text=='Дата регистрации в боте':
         cursor.execute(f'SELECT date FROM users WHERE id = {id}')
-        
         for row in cursor.fetchone():
             bot.send_message(message.chat.id, f'Дата регистрации: {row[2]}')
     else:
